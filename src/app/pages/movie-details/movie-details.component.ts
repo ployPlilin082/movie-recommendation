e716ef89c96095afb2e2ecce4623d1a3901a54dc
ployPlilin082 <114987085+ployPlilin082@users.noreply.github.com>
2026-01-22 00:51:41 +0700

    Improve Ui

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movie.service';
-import {TrailerButtonComponent} from '../../component/trailer-button/trailer-button.component';
+import { TrailerButtonComponent } from '../../component/trailer-button/trailer-button.component';
 import { CommonModule } from '@angular/common';
 import { PlaylistService } from '../../services/playlist.service';
 import { UserHistoryService } from '../../services/๊userhistory.service';
@@ -33,71 +33,71 @@ export class MovieDetailsComponent implements OnInit {
     private movieService: MovieService,
     private playlistService: PlaylistService,
     private userHistoryService: UserHistoryService
-    
-  ) {}
 
-ngOnInit() {
-  this.route.paramMap.subscribe(params => {
-    const id = params.get('id');
-    if (!id) return;
+  ) { }
 
-    this.movieService.getMovieDetail(id).subscribe(res => {
-      this.movie = {
-        ...res,
-        media_type: 'movie'
-      };
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (!id) return;
 
-      if (this.movie.genres) {
-        this.genreText = this.movie.genres
-          .map((g: Genre) => g.name)
-          .join(', ');
-      }
+      this.movieService.getMovieDetail(id).subscribe(res => {
+        this.movie = {
+          ...res,
+          media_type: 'movie'
+        };
+
+        if (this.movie.genres) {
+          this.genreText = this.movie.genres
+            .map((g: Genre) => g.name)
+            .join(', ');
+        }
+      });
     });
-  });
-}
+  }
 
-getShareUrl(postId: number) {
-  return `${window.location.origin}/community/reviews/${postId}`;
-}
-likeMovie() {
-  this.movieService.likeMovie(this.movie.id).subscribe(() => {
-    this.movie.likeCount = (this.movie.likeCount ?? 0) + 1;
-    this.userHistoryService.log({
-    movieId: this.movie.id,
-    interactionTypeId: 2 
-  });
-  });
-}
-
-shareMovie() {
-  const url = `${window.location.origin}/movie/${this.movie.id}`;
-
-  this.movieService.shareMovie(this.movie.id).subscribe(() => {
-    this.movie.shareCount = (this.movie.shareCount ?? 0) + 1;
-  });
-
-  if (navigator.share) {
-    navigator.share({
-      title: this.movie.title,
-      text: 'หนังเรื่องนี้น่าดูมาก 🎬',
-      url
+  getShareUrl(postId: number) {
+    return `${window.location.origin}/community/reviews/${postId}`;
+  }
+  likeMovie() {
+    this.movieService.likeMovie(this.movie.id).subscribe(() => {
+      this.movie.likeCount = (this.movie.likeCount ?? 0) + 1;
+      this.userHistoryService.log({
+        movieId: this.movie.id,
+        interactionTypeId: 2
+      });
     });
-  } else {
-    window.open(
-      `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`,
-      '_blank'
-    );
-    this.userHistoryService.log({
-    movieId: this.movie.id,
-    interactionTypeId: 3 // View
-  });
-}
-}
+  }
 
- getImage(path: string) {
+  shareMovie() {
+    const url = `${window.location.origin}/movie/${this.movie.id}`;
+
+    this.movieService.shareMovie(this.movie.id).subscribe(() => {
+      this.movie.shareCount = (this.movie.shareCount ?? 0) + 1;
+    });
+
+    if (navigator.share) {
+      navigator.share({
+        title: this.movie.title,
+        text: 'หนังเรื่องนี้น่าดูมาก 🎬',
+        url
+      });
+    } else {
+      window.open(
+        `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`,
+        '_blank'
+      );
+      this.userHistoryService.log({
+        movieId: this.movie.id,
+        interactionTypeId: 3 // View
+      });
+    }
+  }
+
+  getImage(path: string) {
     return 'https://image.tmdb.org/t/p/w500' + path;
-}
-addToMyList() {
+  }
+ addToMyList() {
   this.playlistService.addMyItem(this.movie).subscribe({
     next: () => {
       this.toastMessage = `Added "${this.movie.title}" to My List`;
@@ -118,5 +118,4 @@ addToMyList() {
     }
   });
 }
-
 }
